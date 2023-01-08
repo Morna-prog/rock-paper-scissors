a55e1d27e386b72da03cdb54ef73b22dac31abbd
Morna-prog <ChProgram.work@gmail.com>
2023-01-08 10:45:16 -0300

    Modified the logic and added a scoring system, also added 5 rounds each time you reload the page with a winning/losing message inside the console

--- scripts.js
+++ scripts.js
@@ -9,32 +9,51 @@ function getComputerChoice(){
     }
 }
 
-function rockPaperScissors(playerSelection, computerSelection) {
-    playerSelection = prompt("Rock, Paper or scissors").toLowerCase()
-    computerSelection = getComputerChoice()
-
+function playRound(playerSelection, computerSelection) {
     if(playerSelection == "rock" && computerSelection == "scissors" ) {
-        console.log("You win! Rock beats Scissors")
+        playerScore +=1
+        return "You win! Rock beats Scissors"
 
     } else if (playerSelection == "paper" && computerSelection == "rock" ) {
-        console.log("You win! Paper beats Rock")
+        playerScore +=1
+        return "You win! Paper beats Rock"
 
     } else if (playerSelection == "scissors" && computerSelection == "paper" ) {
-        console.log("You win! Scissors beats Paper")
+        playerScore +=1
+        return "You win! Scissors beats Paper"
 
     } else if (playerSelection == "rock" && computerSelection == "paper" ) {
-        console.log("You lose! Paper beats Rock")
+        computerScore +=1
+        return "You lose! Paper beats Rock"
 
     } else if (playerSelection == "scissors" && computerSelection == "rock" ) {
-        console.log("You lose! Rock beats scissors")
+        computerScore +=1
+        return "You lose! Rock beats scissors"
 
     } else if (playerSelection == "paper" && computerSelection == "scissors" ) {
-        console.log("You lose! Scissors beat paper")
+        computerScore +=1
+        return "You lose! Scissors beat paper"
 
     }else if (playerSelection == computerSelection) {
-        console.log("Tie! You selected the same as the computer!")
+        return "Tie! You selected the same as the computer!"
     } else {
-        console.log("Please recharge and input a valid option")
+        return "Please recharge and input a valid option"
     }
 }
-rockPaperScissors()
+let playerScore = 0;
+let computerScore = 0;
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper or scissors").toLowerCase();
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection))
+    }
+    if(playerScore > computerScore) {
+        console.log(`Your score is ${playerScore}, The computer score is ${computerScore}, You win!`)
+    } else if (playerScore < computerScore) {
+        console.log(`Your score is ${playerScore}, The computer score is ${computerScore}, You lose!`)
+    } else {
+        console.log(`Your score is ${playerScore}, The computer score is ${computerScore}, Its a tie!`)
+    }
+}
+game()
